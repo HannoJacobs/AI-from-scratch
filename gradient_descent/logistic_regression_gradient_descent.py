7c67c35d142e6fa0881e914f4249a394052bedf3
Hanno Jacobs <35495246+HannoJacobs@users.noreply.github.com>
2024-09-15 20:56:55 +0200

    black editor

--- gradient_descent/logistic_regression_gradient_descent.py
+++ gradient_descent/logistic_regression_gradient_descent.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x1 = np.random.uniform(-4, 4, 10)
-y1 = 1/(1 + np.exp(-1*(2*x1 + 1 + (1/100)*np.random.randn())))
+y1 = 1 / (1 + np.exp(-1 * (2 * x1 + 1 + (1 / 100) * np.random.randn())))
 
 # y = 1/(1 + np.exp(-1*(a*x1 + b)))
-a = (1/10)*np.random.randn()
-b = (1/10)*np.random.randn()
+a = (1 / 10) * np.random.randn()
+b = (1 / 10) * np.random.randn()
 learning_rate = 0.1
+
 
 def gradient_descent(x1, y1, a, b, learning_rate):
     # loss function for mean squared error of a logistic regressor
@@ -20,31 +21,38 @@ def gradient_descent(x1, y1, a, b, learning_rate):
     num_samples = x1.shape[0]
 
     for x1i, y1i in zip(x1, y1):
-        deriv_loss_wrt_a += (-2*(x1i)*np.exp(a*x1i + b)*((y1i - 1)*np.exp(a*x1i + b) + y1i))/((np.exp(a*x1i + b) + 1)**3)
-        deriv_loss_wrt_b += (-2*(1)*np.exp(a*x1i + b)*((y1i - 1)*np.exp(a*x1i + b) + y1i))/((np.exp(a*x1i + b) + 1)**3)
+        deriv_loss_wrt_a += (
+            -2 * (x1i) * np.exp(a * x1i + b) * ((y1i - 1) * np.exp(a * x1i + b) + y1i)
+        ) / ((np.exp(a * x1i + b) + 1) ** 3)
+        deriv_loss_wrt_b += (
+            -2 * (1) * np.exp(a * x1i + b) * ((y1i - 1) * np.exp(a * x1i + b) + y1i)
+        ) / ((np.exp(a * x1i + b) + 1) ** 3)
 
     # update the logistic regressor parameters
-    a -= learning_rate*(1/num_samples)*deriv_loss_wrt_a
-    b -= learning_rate*(1/num_samples)*deriv_loss_wrt_b
+    a -= learning_rate * (1 / num_samples) * deriv_loss_wrt_a
+    b -= learning_rate * (1 / num_samples) * deriv_loss_wrt_b
 
-    loss = loss = (y1 - 1/(1 + np.exp(-1*(a*x1 + b))))**2
+    loss = loss = (y1 - 1 / (1 + np.exp(-1 * (a * x1 + b)))) ** 2
     total_loss = np.sum(loss, axis=0)
 
     return a, b, total_loss
 
+
 num_epochs = 2000
 for epoch in range(num_epochs):
     a, b, total_loss = gradient_descent(x1, y1, a, b, learning_rate)
-    print(f"a = {np.round(a,2)}\t b = {np.round(b,2)}\t loss = {np.round(total_loss,2)}")
+    print(
+        f"a = {np.round(a,2)}\t b = {np.round(b,2)}\t loss = {np.round(total_loss,2)}"
+    )
 
 print(f"\nlocal_min = {a}, {b}\n")
 
 # plotting original vs the approximated function
 x1 = np.linspace(-4, 4, 100)
-y1 = 1/(1 + np.exp(-1*(2*x1 + 1 + (1/100)*np.random.randn())))
-y_approx = 1/(1 + np.exp(-1*(a*x1 + b)))
-plt.plot(x1, y1, 'b', label='Original function')
-plt.plot(x1, y_approx, 'r', label='Approximated function')
+y1 = 1 / (1 + np.exp(-1 * (2 * x1 + 1 + (1 / 100) * np.random.randn())))
+y_approx = 1 / (1 + np.exp(-1 * (a * x1 + b)))
+plt.plot(x1, y1, "b", label="Original function")
+plt.plot(x1, y_approx, "r", label="Approximated function")
 plt.legend()
 plt.title(f"The approximation after {num_epochs} epochs")
 plt.show()
--- gradient_descent/polynomial_regression_gradient_descent.py
+++ gradient_descent/polynomial_regression_gradient_descent.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 x1 = np.random.uniform(-2, 2, 10)
-y1 = 4.1*x1**3 + 3.1*x1**2 + 2.1*x1 + 1.0 + (1/10)*np.random.randn()
+y1 = 4.1 * x1**3 + 3.1 * x1**2 + 2.1 * x1 + 1.0 + (1 / 10) * np.random.randn()
 
 # y = ax**3 + bx**2 + cx + d
-a = (1/10)*np.random.randn()
-b = (1/10)*np.random.randn()
-c = (1/10)*np.random.randn()
-d = (1/10)*np.random.randn()
+a = (1 / 10) * np.random.randn()
+b = (1 / 10) * np.random.randn()
+c = (1 / 10) * np.random.randn()
+d = (1 / 10) * np.random.randn()
 learning_rate = 0.01
+
 
 def gradient_descent(x1, y1, a, b, c, d, learning_rate):
     # loss function for mean squared error of a polynomial regressor
@@ -23,18 +24,26 @@ def gradient_descent(x1, y1, a, b, c, d, learning_rate):
     num_samples = x1.shape[0]
 
     for x1i, y1i in zip(x1, y1):
-        deriv_loss_wrt_a += -2*(x1i**3)*(y1i - (a*x1i**3 + b*x1i**2 + c*x1i + d))
-        deriv_loss_wrt_b += -2*(x1i**2)*(y1i - (a*x1i**3 + b*x1i**2 + c*x1i + d))
-        deriv_loss_wrt_c += -2*(x1i**1)*(y1i - (a*x1i**3 + b*x1i**2 + c*x1i + d))
-        deriv_loss_wrt_d += -2*(x1i**0)*(y1i - (a*x1i**3 + b*x1i**2 + c*x1i + d))
+        deriv_loss_wrt_a += (
+            -2 * (x1i**3) * (y1i - (a * x1i**3 + b * x1i**2 + c * x1i + d))
+        )
+        deriv_loss_wrt_b += (
+            -2 * (x1i**2) * (y1i - (a * x1i**3 + b * x1i**2 + c * x1i + d))
+        )
+        deriv_loss_wrt_c += (
+            -2 * (x1i**1) * (y1i - (a * x1i**3 + b * x1i**2 + c * x1i + d))
+        )
+        deriv_loss_wrt_d += (
+            -2 * (x1i**0) * (y1i - (a * x1i**3 + b * x1i**2 + c * x1i + d))
+        )
 
-    # make update to our 
-    a -= learning_rate*(1/num_samples)*deriv_loss_wrt_a
-    b -= learning_rate*(1/num_samples)*deriv_loss_wrt_b
-    c -= learning_rate*(1/num_samples)*deriv_loss_wrt_c
-    d -= learning_rate*(1/num_samples)*deriv_loss_wrt_d
+    # make update to our
+    a -= learning_rate * (1 / num_samples) * deriv_loss_wrt_a
+    b -= learning_rate * (1 / num_samples) * deriv_loss_wrt_b
+    c -= learning_rate * (1 / num_samples) * deriv_loss_wrt_c
+    d -= learning_rate * (1 / num_samples) * deriv_loss_wrt_d
 
-    loss = (y1 - (a*x1**3 + b*x1**2 + c*x1 + d))**2
+    loss = (y1 - (a * x1**3 + b * x1**2 + c * x1 + d)) ** 2
     total_loss = np.sum(loss, axis=0)
 
     return a, b, c, d, total_loss
@@ -42,6 +51,8 @@ def gradient_descent(x1, y1, a, b, c, d, learning_rate):
 
 for epoch in range(1000):
     a, b, c, d, total_loss = gradient_descent(x1, y1, a, b, c, d, learning_rate)
-    print(f"a = {np.round(a,2)}\t b = {np.round(b,2)}\t c = {np.round(c,2)}\t d = {np.round(d,2)}\tloss = {np.round(total_loss,2)}")
+    print(
+        f"a = {np.round(a,2)}\t b = {np.round(b,2)}\t c = {np.round(c,2)}\t d = {np.round(d,2)}\tloss = {np.round(total_loss,2)}"
+    )
 
 print(f"\nlocal_min = {a}, {b}, {c}, {d}\n")
